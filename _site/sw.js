ef9733848e200a2c8e8569618f7c1b8809fedc8e
Bosco Martinez <martinez.bosco@gmail.com>
2023-08-03 14:10:35 +0300

    Elos Fest 2023 ajanvaraus

--- _site/sw.js
+++ _site/sw.js
@@ -1,8 +1,8 @@
-const version = '20230731094841';
+const version = '20230803110851';
 const cacheName = `static::${version}`;
 
 const buildContentBlob = () => {
-  return ["/general/2019/08/27/ex3/","/history/external%20sources/2018/08/27/ex2/","/general/external%20sources/2016/08/27/example-post-one/","/ajanvaraus/","/blogi/","/","/minusta/","/assets/styles.css","/yhteystiedot/","/manifest.json","/assets/search.json","/redirects.json","/feed.xml","/sitemap.xml","/robots.txt","/assets/logos/logo64.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
+  return ["/general/2019/08/27/ex3/","/history/external%20sources/2018/08/27/ex2/","/general/external%20sources/2016/08/27/example-post-one/","/ajanvaraus/","/blogi/","/elos/","/","/minusta/","/assets/styles.css","/yhteystiedot/","/manifest.json","/assets/search.json","/redirects.json","/feed.xml","/sitemap.xml","/robots.txt","/assets/logos/logo64.png", "/assets/default-offline-image.png", "/assets/scripts/fetch.js"
   ]
 }
 
---
+++ elos.md
@@ -0,0 +1,6 @@
+---
+title: "Ajanvaraus - Elos Fest 2023"
+excerpt: "Thaijoogahieronnan ajanvaraus"
+---
+
+<iframe src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ1GcaWBKiEPnEjBNQmHcGBowE9v9NmAd7405e_tUaz9ja5favWL94s6uC5PZnKw60eZ3OTyTpUd?gv=true" style="border: 0" width="100%" height="600" frameborder="0"></iframe>
